4f4f603ecb284729b797dce2e7b4ab1e9a93f8c0
Justine Gounga <justine.gounga@gmail.com>
2025-12-13 04:50:59 -0500

    added documentation throughout all pages

--- Project 1/js/feedback.js
+++ Project 1/js/feedback.js
@@ -1,15 +1,23 @@
+
+
+// Wait until the DOM is ready to be manipulated
 document.addEventListener("DOMContentLoaded", function () {
 
+  // yes radio button
   const yesOption = document.querySelector("#yes");
 
+  // no radio button
   const noOption = document.querySelector("#no");
 
+  // text to display thank you message
   const message = document.querySelector("#ratingMessage");
 
+  // show message when yes is clicked 
   yesOption.addEventListener("click", function () {
     message.textContent = "Thanks! We are glad you recommend it ❤︎"; //heart icon from https://emojicombos.com/heart
   });
 
+  // show message when no is clicked 
   noOption.addEventListener("click", function () {
     message.textContent = "Thanks! We appreciate the feedback. ❤︎"; //heart icon from https://emojicombos.com/heart
   });
--- Project 1/js/gallery.js
+++ Project 1/js/gallery.js
@@ -1,36 +1,39 @@
 
 
+// Wait until the DOM is ready to be manipulated
 document.addEventListener("DOMContentLoaded", function () {
 
-
+    // gets the ul list with the product links 
     const productGallery = document.getElementById("productGallery");
 
-
+    // contain all the A elements 
     const productLinks = document.querySelectorAll("#productGallery a");
 
+    // loop through each product link in the gallery
     for (let i = 0; i < productLinks.length; i++) {
 
+        // add click event listener
         productLinks[i].addEventListener("click", function (event) {
 
-
+            // get the value of the href attribute
             let productHref = productLinks[i].getAttribute("href");
-            console.log(productHref);
 
-
+            // main image element that displays the product
             const productImage = document.getElementById("productImage");
 
+            // update the image source to show the selected product
             productImage.setAttribute("src", productHref);
 
-
+            // get the product description
             let productDescription = this.getAttribute("title");
-            console.log(productDescription);
 
-
+            // select the paragrpah when the product description is displayed
             const productDescriptionParagraph = document.querySelector("#productDescription");
 
+            // update the paragraph text with the selected product 
             productDescriptionParagraph.textContent = productDescription;
 
-
+            // cancels the default behaviour of each hyperlink
             event.preventDefault();
         });
     }
